98837cdba6e4c0e9f33bdeeee19d896bdf2a5dab
joanna_Cin <joanna@cinnamon.is>
2021-08-16 15:40:05 +0000

    Fix metrics

--- train/metrics.py
+++ train/metrics.py
@@ -1,17 +1,11 @@
-import numpy as np
-from sklearn.metrics import f1_score, ndcg_score
+from torchmetrics.classification import F1, Precision
+from sklearn.metrics import ndcg_score
 
-def precision(y_true, y_prob, k: int):
-    ky_pred = np.argsort(y_prob)[::-1][:, :k]
-    score = y_true.take(ky_pred).sum(axis=1) / k
-    return score.mean()
 
-def compute_metrics(y_true: np.ndarray, y_prob: np.ndarray, threshold: float = 0.5):
+def compute_metrics(y_true, y_prob, threshold: float = 0.5):
     metrics = {}
-    y_pred = np.where(y_prob >= threshold, 1, 0)
-    # y_pred = np.argmax(y_prob, axis=1)
-    metrics["micro_f1"] = f1_score(y_true, y_pred, average="micro")
+    metrics["micro_f1"] = F1(threshold=threshold, average="micro")(y_prob, y_true).item()
     for k in (1, 3, 5):
-        metrics[f"p@{k}"] = precision(y_true, y_prob, k=k)
+        metrics[f"p@{k}"] = Precision(threshold=threshold, average="micro", top_k=k)(y_prob, y_true).item()
         metrics[f"ndcg@{k}"] = ndcg_score(y_true, y_prob, k=k)
     return metrics

84423b09c1ec0bfdbf2f64072d048d3b4543711c
joanna_Cin <joanna@cinnamon.is>
2021-08-24 01:52:12 +0000

    Flexible epochs for active learning

--- train/strategy.py
+++ train/strategy.py
@@ -2,30 +2,33 @@ import torch
 import numpy as np
 from math import ceil
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-def mask2idx(mask: torch.BoolTensor) -> np.array:
+def mask2idx(mask: torch.BoolTensor) -> np.ndarray:
     return torch.where(mask)[0].cpu().numpy()
 
 class Strategy:
-    def __init__(self, unlabel_mask: torch.BoolTensor, n_rounds: int = 10):
+    def __init__(self, unlabel_mask: torch.BoolTensor, n_rounds: int = 100):
         self.unlabel_mask = unlabel_mask
         self.n_samples = len(self.unlabel_mask)
         self.train_mask = torch.zeros(self.n_samples, dtype=torch.bool)
         self.n_samples_per_round = ceil(sum(self.unlabel_mask).item() / n_rounds)
+        self.randomizer = np.random.RandomState(seed=1)
 
-    def init_random_mask(self):
+    def random_mask(self):
         train_idx = mask2idx(self.unlabel_mask)
-        rand_idx = np.random.choice(train_idx, size=self.n_samples_per_round)
+        rand_idx = self.randomizer.choice(train_idx, size=self.n_samples_per_round)
         self.train_mask[rand_idx] = True
         self.unlabel_mask[rand_idx] = False
         return self.train_mask
 
-    def query(self, probs):
+    def query(self, probs: torch.Tensor = None):
         pass
 
+class RandomSampling(Strategy):
+    def query(self, probs: torch.Tensor = None):
+        return super().random_mask()
+
 class LeastConfidence(Strategy):
-    def query(self, probs: torch.Tensor):
+    def query(self, probs: torch.Tensor = None):
         confidence = probs[self.unlabel_mask].max(dim=1).values
         unlabel_idx = torch.arange(self.n_samples)[self.unlabel_mask]
         select_idx = unlabel_idx[confidence.sort().indices[:self.n_samples_per_round]]
@@ -33,8 +36,10 @@ class LeastConfidence(Strategy):
         self.unlabel_mask[select_idx] = False
         return self.train_mask
 
-def prepare_strategy(strategy_name: str, unlabel_mask: torch.BoolTensor):
+def prepare_strategy(strategy_name: str, unlabel_mask: torch.BoolTensor, n_rounds: int = 100):
     if strategy_name == "lc":
-        return LeastConfidence(unlabel_mask)
+        return LeastConfidence(unlabel_mask, n_rounds)
+    elif strategy_name == "random_sampling":
+        return RandomSampling(unlabale_mask, n_rounds)
     else:
         raise NotImplementedError
--- train/train.py
+++ train/train.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from tqdm import trange
 
 import torch
@@ -7,46 +8,71 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
 from dgl import DGLGraph
+
 from .strategy import *
 from .metrics import compute_metrics
 
-
-def train(model: nn.Module, graph: DGLGraph, target_node: str, lr: float, epochs: int, threshold: float = 0.5, strategy_name: str = "lc", writer: SummaryWriter = None, exp_name: str = "test"):
+def train(
+        model: nn.Module, graph: DGLGraph, target_node: str, 
+        lr: float, epochs: int, threshold: float = 0.5, strategy_name: str = "lc", 
+        writer: SummaryWriter = None, exp_name: str = "test"
+    ):
     criterion = nn.BCEWithLogitsLoss()
     optimizer = optim.AdamW(model.parameters(), lr=lr)
-    lr_scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda epoch : lr * 0.9 ** (epoch - 1))
+    lr_scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda it : lr * 0.9 ** (it - 1))
     train_mask = graph.nodes[target_node].data["train_mask"]
     val_mask = graph.nodes[target_node].data["val_mask"]
     test_mask = graph.nodes[target_node].data["test_mask"]
     labels = graph.nodes[target_node].data["label"]
 
-    pbar = trange(epochs, desc="Training")
-    strategy = prepare_strategy(strategy_name, train_mask)
-    query_train_mask = strategy.init_random_mask()
-    for epoch in pbar:
-        # train step
-        optimizer.zero_grad()
-        logits = model(graph, graph.ndata["feat"], target_node, edge_weight=graph.edata["weight"])
-        loss = criterion(logits[query_train_mask], labels[query_train_mask].type_as(logits))
-        loss.backward()
-        optimizer.step()
-        pbar.set_postfix(loss=loss.item(), lr="{:.1e}".format(lr_scheduler.get_last_lr()[0]))
-        writer.add_scalar("loss/train", loss.item(), epoch)
-        # choose next samples
-        probs = torch.sigmoid(logits.detach())
-        query_train_mask = strategy.query(probs)
-        if (epoch + 1) % 100 == 0:
-            val_scores = eval(labels, logits, val_mask, threshold=threshold)
-            print(val_scores)
-            for k, v in val_scores.items():
-                writer.add_scalar(f"{k}/val", v, epoch)
-            lr_scheduler.step()
+    use_active_learning = True
+    if use_active_learning:
+        n_rounds = 100
+        round_epoch = epochs // n_rounds
+        strategy = prepare_strategy(strategy_name, train_mask, n_rounds)
+        query_train_mask = strategy.random_mask()
+        update_freq = min(10, round_epoch)
+    else:
+        n_rounds = 1
+        round_epoch = epochs
+        query_train_mask = train_mask
+        update_freq = 100
 
+    iteration = 0
+    for round in range(n_rounds):
+        logging.info(f"START ROUND {round + 1}")
+        pbar = trange(round_epoch, desc="Training")
+        for epoch in pbar:
+            iteration += 1
+            # train step
+            optimizer.zero_grad()
+            logits = model(graph, graph.ndata["feat"], target_node, graph.edata["weight"])
+            loss = criterion(logits[query_train_mask], labels[query_train_mask].type_as(logits))
+            loss.backward()
+            optimizer.step()
+            pbar.set_postfix(loss=loss.item(), lr="{:.1e}".format(lr_scheduler.get_last_lr()[0]))
+            writer.add_scalar("loss/train", loss.item(), epoch)
+
+            # choose next samples
+            if use_active_learning and round != (n_rounds - 1):
+                probs = torch.sigmoid(logits.detach().cpu())
+                query_train_mask = strategy.query(probs)
+
+            if (epoch + 1) % update_freq == 0:
+                val_scores = eval(labels, logits, val_mask, threshold=threshold)
+                print(val_scores)
+                for k, v in val_scores.items():
+                    writer.add_scalar(f"{k}/val", v, iteration)
+
+            if iteration % 100 == 0:
+                lr_scheduler.step()
+
+    # inference at the last round
     print("**** TEST ****")
     test_scores = eval(labels, logits, test_mask, threshold=threshold)
     print(test_scores)
     for k, v in test_scores.items():
-        writer.add_scalar(f"{k}/test", v, epoch)
+        writer.add_scalar(f"{k}/test", v, 0)
 
     os.makedirs("results", exist_ok=True)
     report = pd.Series(test_scores).sort_index() * 100

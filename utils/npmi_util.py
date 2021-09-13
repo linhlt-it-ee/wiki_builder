317b0ecb797a5bb78ed47da669bb72f69dc6ea7a
joanna_Cin <joanna@cinnamon.is>
2021-09-13 04:37:04 +0000

    Refactor PMI

--- utils/npmi_util.py
+++ utils/npmi_util.py
@@ -9,33 +9,41 @@ import fugashi
 import numpy as np
 from tqdm import tqdm
 from nltk.stem import porter, WordNetLemmatizer
+from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 from . import file_utils
 
-def normalize_text(doc_content_list: List[str], lang: str = "en", cache_dir="./tmp"):
-    res = []
-    stemmer = porter.PorterStemmer()
-    lemmatizer = WordNetLemmatizer()
-    tagger = fugashi.Tagger()
-    for doc in tqdm(doc_content_list, desc="Normalizing"):
+def normalizer(lang: str = "en"):
+    if lang == "en":
+        stemmer = porter.PorterStemmer()
+        lemmatizer = WordNetLemmatizer()
+    elif lang == "ja":
+        tagger = fugashi.Tagger()
+    else:
+        raise NotImplementedError
+
+    def fn(doc):
         if lang == "en":
             words = [w for w in nltk.wordpunct_tokenize(doc) if w.isalpha() and len(w) > 2]
-            words = [lemmatizer.lemmatize(x, "v") for x in words]
-            words = [lemmatizer.lemmatize(x, "n") for x in words]
-            words = [stemmer.stem(x) for x in words]
+            words = [lemmatizer.lemmatize(w, "v") for w in words]
+            words = [lemmatizer.lemmatize(w, "n") for w in words]
+            words = [stemmer.stem(w) for w in words]
         elif lang == "ja":
             words = [str(w.feature.lemma) for w in tagger(doc)]
             words = [w for w in words if w.isalpha() and w != "None"]
-        else:
-            raise NotImplementedError
-        res.append(" ".join(words).lower())
-    file_utils.dump(res, os.path.join(cache_dir, f"normalized_text_{lang}.pck"))
+        return " ".join(words).lower()
+        
+    return fn
 
+def normalize_text(doc_content_list: List[str], lang: str = "en", cache_dir="./tmp"):
+    transformer = normalizer(lang=lang)
+    res = [transformer(doc) for doc in tqdm(doc_content_list, desc="Normalizing")]
+    file_utils.dump(res, os.path.join(cache_dir, f"normalized_text_{lang}.pck"))
     return res
 
 def get_tfidf_score(texts, lang: str = "en", cache_dir="./tmp"):
-    stop_words = "english" if lang == "en" else None
+    stop_words = None if lang != "en" else "english"
     vectorizer = TfidfVectorizer(tokenizer=lambda x : x.split(), stop_words=stop_words, min_df=10, max_df=0.7)
     X = vectorizer.fit_transform(texts)
     tf_vocab = vectorizer.vocabulary_
@@ -47,59 +55,29 @@ def get_tfidf_score(texts, lang: str = "en", cache_dir="./tmp"):
 
 def get_pmi(doc_content_list: List[str], vocab: List[str], window_size: int = 20):
     vocab = {w : i for i, w in enumerate(vocab)}
-    word_freq = defaultdict(lambda : 0)
-    for doc_words in doc_content_list:
-        for word in doc_words.split():
-            if word in vocab:
-                word_freq[word] += 1
-
-    assert len(word_freq) == len(vocab), f"Vocab size mismatch: {len(word_freq)} != {len(vocab)}"
-    word_doc_list = defaultdict(list)
-    for i in range(len(doc_content_list)):
-        doc_words = doc_content_list[i]
-        words = doc_words.split()
-        appeared = set()
-        for word in doc_words.split():
-            if word in appeared or word not in vocab:
-                continue
-            if word in word_doc_list:
-                word_doc_list[word].append(i)
-            appeared.add(word)
-
-    word_doc_freq = {}
-    for word, doc_list in word_doc_list.items():
-        word_doc_freq[word] = len(doc_list)
-
-    # word co-occurence within context windows
+    doc_word_list = [[w for w in x.split() if w in vocab] for x in doc_content_list]
     windows = []
-    for doc_words in tqdm(doc_content_list, desc="Striding"):
-        words = [x for x in doc_words.split() if x in vocab]
+    for words in doc_word_list:
         length = len(words)
         if length <= window_size:
             windows.append(words)
         else:
-            # print(length, length - window_size + 1)
             for j in range(length - window_size + 1):
                 window = words[j: j + window_size]
                 windows.append(window)
 
     word_window_freq = defaultdict(lambda : 0)
-    for window in tqdm(windows, desc="Counting words within window"):
-        appeared = set()
-        for i in range(len(window)):
-            if window[i] in appeared:
-                continue
-            word_window_freq[window[i]] += 1
-            appeared.add(window[i])
+    for window in tqdm(windows, desc="Word frequency (windows)"):
+        for w in set(window):
+            word_window_freq[w] += 1
 
     word_pair_count = defaultdict(lambda : 0)
-    for window in tqdm(windows, desc="Counting word pairs within window"):
+    for window in tqdm(windows, desc="Word pairs frequency (windows)"):
         for i in range(1, len(window)):
             for j in range(i):
                 wi, wj = window[i], window[j]
                 if wi == wj:
                     continue
-                # two orders
                 word_pair_count[(wi, wj)] += 1
                 word_pair_count[(wj, wi)] += 1
 
@@ -109,8 +87,8 @@ def get_pmi(doc_content_list: List[str], vocab: List[str], window_size: int = 20
     for (wi, wj), count in word_pair_count.items():
         word_freq_i = word_window_freq[wi]
         word_freq_j = word_window_freq[wj]
-        pmi = log((1.0 * count / num_window) /
-                  (1.0 * word_freq_i * word_freq_j / (num_window * num_window)))
+        # pmi = log[p(i,j) / p(i)p(j)] = log[n * n(i,j) / n(i)n(j)]
+        pmi = log((1.0 * num_window * count / (word_freq_i * word_freq_j)))
         if pmi <= 0:
             continue
         pmi_word_word[vocab[wi]][vocab[wj]] = pmi
